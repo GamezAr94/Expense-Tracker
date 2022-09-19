a1bdf7f8a027fd1b35e77494c148ab101bbdaee5
Arturo Gamez <arturogamez51@gmail.com>
2022-09-18 18:20:13 -0700

    adding the filter by year and passing the value to its parent component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from '../ExpenseFilter/ExpenseFilter';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
       return (
-        <Card className="expenses">
-          <h2>Let's get started!</h2>
-          < ExpenseItem item={props.items[0]} />
-          < ExpenseItem item={props.items[1]} />
-          < ExpenseItem item={props.items[2]} />
-          < ExpenseItem item={props.items[3]} />
-        </Card>
+        <div>
+          <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+            <h2>Let's get started!</h2>
+            < ExpenseItem item={props.items[0]} />
+            < ExpenseItem item={props.items[1]} />
+            < ExpenseItem item={props.items[2]} />
+            < ExpenseItem item={props.items[3]} />
+          </Card>
+        </div>
       );
 }
 
